aff7d1ce2d89931ab227ef1a0d9fcfdcacf32e84
Jérôme Abel <contact@jeromeabel.net>
2024-05-30 21:44:57 +0200

    feat: fetch projects

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+import { type Project } from '@/data/types';
+const { data : projects} = await useFetch<Project[]>('http://localhost:3001/projects')
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Projects</h1>
+    <div>
+    <div v-for="project in projects" :key="project.id">
+      <b>{{ project.title }}</b>
+      <p>{{ project.date }}</p>
+    </div>
+    
+    
+    </div>
   </div>
 </template>
