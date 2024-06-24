1ccc34f41f288905eac0b8e4f5d76ee2ec7d99c8
Jérôme Abel <contact@jeromeabel.net>
2024-06-24 13:14:15 +0200

    feat: add a BaseError component

---
+++ components/base/BaseError.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { AlertCircle } from 'lucide-vue-next'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+
+defineProps<{ error?: string }>()
+</script>
+
+<template>
+  <Alert variant="destructive" data-test="alert-error">
+    <AlertCircle class="w-4 h-4" />
+    <AlertTitle>Error</AlertTitle>
+    <AlertDescription>{{ error }}</AlertDescription>
+  </Alert>
+</template>
