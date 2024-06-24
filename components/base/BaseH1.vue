c83a0d77b7e8da1e2a9931256c436b300dab4c8f
Jérôme Abel <contact@jeromeabel.net>
2024-06-24 13:14:25 +0200

    style: style H1

--- components/base/BaseH1.vue
+++ components/base/BaseH1.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-  <slot />
-</h1> 
+  <h1 class="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+    <slot />
+  </h1>
 </template>
