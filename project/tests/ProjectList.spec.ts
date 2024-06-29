88c07a177c60337b985ba17ed38e902ff138c3fe
Jérôme Abel <contact@jeromeabel.net>
2024-06-29 08:51:39 +0200

    test: add ProjectList tests

--- project/tests/ProjectList.spec.ts
+++ project/tests/ProjectList.spec.ts
@@ -1,14 +1,37 @@
 import { describe, it, expect } from 'vitest'
 import { mountSuspended } from '@nuxt/test-utils/runtime' // => Icon component which could be async
 
-import { type Project, ProjectList, PROJECTS_NB, NO_PROJECTS_MESSAGE } from '@/project'
+import { ProjectList, generateMockProjects, PROJECTS_NB, NO_PROJECTS_MESSAGE } from '@/project'
 
 describe('ProjectList', () => {
-  it.todo('Should render N project items when its number is greater than N')
-  it.todo('Should render the correct number of project items when its number is lesser than N');
-  it.todo('Should display N skeleton projects on loading state')
-  it.todo('Should display a message when the array of projects is empty')
-  it.todo('Should display a message when no projects props are specified')
-  it.todo('Should display a warning component when no projects are present')
-  it.todo('Should not display a warning component when projects are present')
+  it('Should render correct number of project items when loading is false', async () => {
+    const projects = generateMockProjects(5);
+    const wrapper = await mountSuspended(ProjectList, { props: { projects, loading: false } });
+    const projectItems = wrapper.findAll('[data-test="project-item"]');
+    
+    expect(projectItems.length).toBe(projects.length);
+  });
+
+  it('Should display N skeleton projects on loading state', async () => {
+    const wrapper = await mountSuspended(ProjectList, { props: { loading: true }});
+    const skeletonItems = wrapper.findAll('[data-test="project-skeleton"]');
+
+    expect(skeletonItems.length).toBe(PROJECTS_NB);
+  })
+
+  it('Should display warning component with a message when no projects are specified', async () => {
+    const wrapper = await mountSuspended(ProjectList);
+    const warning = wrapper.find('[data-test="project-warning"]');
+
+    expect(warning.exists()).toBe(true);
+    expect(warning.text()).toContain(NO_PROJECTS_MESSAGE);
+  });
+
+  it('Should display display warning component with a message when the project array is empty', async () => {
+    const wrapper = await mountSuspended(ProjectList, { props: { projects: [] } });
+    const warning = wrapper.find('[data-test="project-warning"]');
+
+    expect(warning.exists()).toBe(true);
+    expect(warning.text()).toContain(NO_PROJECTS_MESSAGE)
+  });
 })
