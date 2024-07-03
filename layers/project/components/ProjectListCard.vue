de32d1ddea528d53a85778b9342989e3232880ec
Jérôme Abel <contact@jeromeabel.net>
2024-07-03 08:20:33 +0200

    test: ProjectListCard render details

--- layers/project/components/ProjectListCard.vue
+++ layers/project/components/ProjectListCard.vue
@@ -8,7 +8,7 @@ defineProps<{ project: ProjectSummary }>()
     {{ project.name }}
     {{ project.description }}
     {{ project.stars }}
-
+    {{ project.workflow }}
     <ul>
       <li v-for="tech in project.stack">
         {{ tech }}
--- layers/project/tests/ProjectListCard.spec.ts
+++ layers/project/tests/ProjectListCard.spec.ts
@@ -4,18 +4,19 @@ import ProjectListCard from '../components/ProjectListCard.vue';
 import { type ProjectSummary } from '../types/project.types';
 
 describe('ProjectListCard', () => { 
-    const project: ProjectSummary = { id: 1, name: 'NAME', description: 'DESCRIPTION', stars: 10, workflow: 'FLOW', stack: ['STACK1', 'STACK2']}
+    const project: ProjectSummary = { id: 1, name: 'NAME', description: 'DESCRIPTION', stars: 10, workflow: 'WORKFLOW', stack: ['STACK1', 'STACK2']}
 
     it('Should render the details of the project', async () => {
       const wrapper = await mountSuspended(ProjectListCard, { props: { project }})
       expect(wrapper.text()).toContain('NAME');
       expect(wrapper.text()).toContain('DESCRIPTION');
       expect(wrapper.text()).toContain('10');
+      expect(wrapper.text()).toContain('WORKFLOW');
       expect(wrapper.text()).toContain('STACK1');
       expect(wrapper.text()).toContain('STACK2');
     });
  })
 
-
-    // Should render Stars
+    // Should render Star Button
     // Should render Badges for stack
+    // Should link to a Project Page
