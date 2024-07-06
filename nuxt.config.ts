9957efcb96c04d59140c45e0074a8fc463146b4f
Jérôme Abel <contact@jeromeabel.net>
2024-07-06 07:31:09 +0200

    chore: add shadcn/tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
   future: {
     compatibilityVersion: 4,
   },
-
   compatibilityDate: "2024-07-03",
-  modules: [
-    '@nuxt/test-utils/module'
-  ]
+  modules: ['@nuxt/test-utils/module', "@nuxtjs/tailwindcss", "shadcn-nuxt"]
 })
