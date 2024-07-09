82a67b7cc15602da44e4073ca1be709602471f37
Jérôme Abel <contact@jeromeabel.net>
2024-07-09 07:07:52 +0200

    feat(search): add search in description

--- layers/project/composables/useProjects.ts
+++ layers/project/composables/useProjects.ts
@@ -9,9 +9,9 @@ export const useProjects = (initialProject: Project[]) => {
 
     const filteredProjects = computed(() => {
         if (!searchTerm.value) return projects.value;
-        return projects.value.filter(project =>
-          project.name.toLowerCase().includes(searchTerm.value.toLowerCase())
-        );
+        return projects.value.filter(project => {
+          return project.name.toLowerCase().includes(searchTerm.value.toLowerCase()) || project.description.toLowerCase().includes(searchTerm.value.toLowerCase())
+        });
       });
 
     return {
--- layers/project/tests/useProjects.spec.ts
+++ layers/project/tests/useProjects.spec.ts
@@ -1,14 +1,25 @@
-
-import { describe, it, expect } from 'vitest'
-import { useProjects } from '../composables/useProjects'
-import { mockProjects } from '../utils/project.mock'
+import { describe, it, expect } from 'vitest';
+import { useProjects } from '../composables/useProjects';
+import { type Project } from "../types/project.types";
 
 describe("useProjects", () => {
 
-    it("Should return the correct number of filtered projects", ()=> {
-         const { filteredProjects, updateSearchTerm } = useProjects(mockProjects)
-         expect(filteredProjects.value.length).toEqual(mockProjects.length)
-         updateSearchTerm('commerce')         
-         expect(filteredProjects.value.length).toEqual(2)
-    })
-})
+    it("Should return the correct number of filtered projects based on name and description in lowercase", () => {
+
+        const projects: Project[] = [
+            { name: "Redesign", description: "Website redesign." } as Project,
+            { name: "WEB App", description: "Create web app." } as Project,
+        ];
+        const { filteredProjects, updateSearchTerm } = useProjects(projects);
+
+        // Ensure all projects are included at startup
+        expect(filteredProjects.value.length).toEqual(projects.length);
+
+        // Should filter to only include projects with 'web' or 'design' in name or description
+        updateSearchTerm('web');
+        expect(filteredProjects.value.length).toEqual(2);
+        updateSearchTerm('design');
+        expect(filteredProjects.value.length).toEqual(1);
+    });
+});
+
