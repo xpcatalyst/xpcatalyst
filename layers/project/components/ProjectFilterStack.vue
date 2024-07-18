6da71d53fbd4abc5a511c6048d6484a950520a46
Jérôme Abel <contact@jeromeabel.net>
2024-07-18 23:27:25 +0200

    fix: filter stack

--- layers/project/components/ProjectFilterStack.vue
+++ layers/project/components/ProjectFilterStack.vue
@@ -4,30 +4,35 @@ import { Button } from '@/components/ui/button'
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from '@/components/ui/command'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { Separator } from '@/components/ui/separator'
-import { cn } from '@/lib/utils'
+// import { cn } from '@/lib/utils'
+import { Checkbox } from '@/components/ui/checkbox'
+import { ChevronDown } from 'lucide-vue-next'
+
 // import { type Option } from '../types/project.types.js'
 //const selectedValues = new Set(["React", "Vue", "Angular"])
-const title = "Select Tags"
+const title = "Select a Tech"
 
 type Option = {
-  label: string
-  value: string
+  label: string,
+  value: string,
+  nb: number
 }
 const options: Option [] = [
-  { label: "React", value: "react" },
-  { label: "Vue", value: "vue" },
-  { label: "Angular", value: "angular" },
-  { label: "Vercel", value: "vercel" },
-  { label: "Astro", value: "astro" },
-  { label: "Nuxt", value: "nuxt" }
+  { label: "React", value: "react", nb: 12 },
+  { label: "Vue", value: "vue", nb: 2 },
+  { label: "Angular", value: "angular", nb: 4 },
+  { label: "Vercel", value: "vercel", nb: 5 },
+  { label: "Astro", value: "astro", nb: 1 },
+  { label: "Nuxt", value: "nuxt", nb: 6 }
 ]
 
-const selectedOptions = ref<Option []>([])
-const openMultipleSelect = ref<boolean>(false);
+const selectedOptions = ref<Option[]>([])
 
 const isOptionSelected = (option: Option): boolean => {
-  return selectedOptions.value?.includes(option);
+  return selectedOptions.value?.some(it => it.value === option.value);
 };
+
+const handleClear = () => selectedOptions.value = []
 
 const handleOptionUnselected = (option: Option): void => {
   selectedOptions.value = selectedOptions.value.filter(i => i !== option);
@@ -35,48 +40,46 @@ const handleOptionUnselected = (option: Option): void => {
 
 const handleOptionSelected = (option: Option): void => {
   if( !isOptionSelected(option) ) selectedOptions.value.push(option)
+  else selectedOptions.value = selectedOptions.value.filter(i => i.value !== option.value);  
 };
-
 </script>
 
 <template>
   <Popover>
-    <PopoverTrigger as-child>
+    <PopoverTrigger as-child class="hover:bg-transparent px-3">
       <Button variant="outline">{{ title }}
-
-        <!-- <template v-if="selectedValues.length > 0">
+        <template v-if="selectedOptions.length > 0">
           <Separator orientation="vertical" class="mx-2 h-4" />
           <Badge
             variant="secondary"
             class="rounded-sm px-1 font-normal lg:hidden"
           >
-            {{ selectedValues.size }}
+            {{ selectedOptions.length }}
           </Badge>
           <div class="hidden space-x-1 lg:flex">
             <Badge
-              v-if="selectedValues.size > 2"
-              variant="secondary"
+              v-if="selectedOptions.length > 4"
+              variant="outline"
               class="rounded-sm px-1 font-normal"
             >
-              {{ selectedValues.size }} selected
+              {{ selectedOptions.length }} selected
             </Badge>
-
             <template v-else>
               <Badge
-                v-for="option in selectedValue"
+                v-for="option in selectedOptions"
                 :key="option.value"
-                variant="secondary"
-                class="rounded-sm px-1 font-normal"
+                variant="outline"
+                class="rounded-sm px-1 font-normal flex space-x-1 hover:bg-secondary"
+                @click.stop="handleOptionUnselected(option)"
               >
-                {{ option.label }}
+               <span>{{ option.label }}</span>
+               <Icon name="uil:times" class="size-4" />
               </Badge>
             </template>
           </div>
-        </template> -->
-
-        <Badge v-for="option in selectedOptions" variant="secondary"
-        class="rounded-sm px-1 font-normal":key="option.value" @click="handleOptionUnselected(option)">{{ option.label }}</Badge>
-      </Button>
+        </template> 
+        <ChevronDown v-else class="w-4 h-4 ms-3 opacity-50" />
+    </Button> 
     </PopoverTrigger>
     <PopoverContent class="w-[200px] p-0" align="start">
       <Command>
@@ -84,21 +87,24 @@ const handleOptionSelected = (option: Option): void => {
         <CommandList>
           <CommandEmpty>No results found.</CommandEmpty>
           <CommandGroup>
-            <CommandItem v-for="option in options" :key="option.value" :value="option.value"
-              @select="handleOptionSelected(option)">
-              <div
-                :class="cn(
-                  'mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary',
-                  isOptionSelected(option)
-                    ? 'bg-primary text-primary-foreground'
-                    : 'opacity-50 [&_svg]:invisible',
-                )"
-              >
-                <Icon name="uil:check" class="size-4" />
-              </div>
-              <span>{{ option.label }}</span>
+            <CommandItem  class="flex items-center space-x-2" v-for="option in options" :key="option.value" :value="option.value" @select="handleOptionSelected(option)">
+              <Checkbox id="option.value" :checked="isOptionSelected(option)"/>
+              <label>{{ option.label }} ({{ option.nb }})</label>
             </CommandItem>
           </CommandGroup>
+
+          <template v-if="selectedOptions.length > 0">
+            <CommandSeparator />
+            <CommandGroup>
+              <CommandItem
+                :value="{ label: 'Clear filters' }"
+                class="justify-center text-center"
+                @select="handleClear"
+              >
+                Clear filters
+              </CommandItem>
+            </CommandGroup>
+          </template>
         </CommandList>
       </Command>
     </PopoverContent>
