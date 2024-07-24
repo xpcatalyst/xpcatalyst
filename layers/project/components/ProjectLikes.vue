c41d584d6e1ad7aca15b2b6e348d45980f30de16
Jérôme Abel <contact@jeromeabel.net>
2024-07-24 07:37:12 +0200

    ui: add heart icon to likes

--- layers/project/components/ProjectLikes.vue
+++ layers/project/components/ProjectLikes.vue
@@ -10,7 +10,8 @@ const handleLike = () => {
 
 <template>
   <button class="flex items-center text-xs gap-1" :class="{ 'text-pink-500': isLiked }"  @click="handleLike">
-    <Icon :name="isLiked ? 'mdi:star' : 'mdi:star-outline'" size="24" />
+    <Icon v-if="!isLiked" name="mdi:heart-outline" size="24" />
+    <Icon v-else name="mdi:heart" size="24" />
     <span>{{ likes ?? 0 }}</span>
   </button>
 </template>
