d63c7d678c401a2a31601006fa27254fdccff089
Jérôme Abel <contact@jeromeabel.net>
2024-09-11 07:17:43 +0200

    chore: change build directory to dist to align with netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  buildDir: 'dist',
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
