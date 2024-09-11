f7726fd7dc62b5e74fdc2da49ac51dc2bf1fd516
Jérôme Abel <contact@jeromeabel.net>
2024-09-11 07:26:35 +0200

    test: deploy with output/public directory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  buildDir: 'dist',
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
