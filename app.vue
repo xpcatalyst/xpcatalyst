067098c662f16886a3b241c05c93d061fb634668
Jérôme Abel <contact@jeromeabel.net>
2024-10-10 05:18:06 +0200

    chore: eslint

--- app.vue
+++ app.vue
@@ -2,5 +2,16 @@
   <NuxtLayout>
     <NuxtLoadingIndicator />
     <NuxtWelcome />
+
+    <p>hjihoh
+
+
+    </p>
+
+
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const unusedVar = 'test';
+</script>
