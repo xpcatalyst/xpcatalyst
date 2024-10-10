0eac1c9e8b8a6500a7a722afc2840b6aa44ec681
Jérôme Abel <contact@jeromeabel.net>
2024-10-10 06:24:40 +0200

    chore: use eslint stylistic rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      stylistic: true
+    },
+  }
 })
