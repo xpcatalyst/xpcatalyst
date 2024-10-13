8a77508b66270ea31d73fd33d13451b586ac4513
Jérôme Abel <contact@jeromeabel.net>
2024-10-13 06:39:03 +0200

    layout: change background image to home page only

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,18 +1,9 @@
 <template>
-  <div class="grid min-h-screen grid-rows-[auto_1fr_auto] bg-gradient-to-tr from-xp-light-green">
+  <div class="grid min-h-screen grid-rows-[auto_1fr_auto] ">
     <AppHeader />
     <main class="flex flex-col">
       <slot />
     </main>
     <AppFooter />
-    <NuxtImg
-      src="/images/hero.jpg"
-      alt="Background Image"
-      class="absolute inset-0 w-full h-full object-cover -z-10 opacity-50 blur-sm"
-      format="webp"
-      sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
-      :width="1920"
-      :height="1080"
-    />
   </div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,13 @@
 <template>
   <HeroSection />
+  <NewsletterForm />
+  <NuxtImg
+    src="/images/hero2.jpg"
+    alt="Background Image"
+    class="absolute inset-0 w-full h-full object-cover -z-10 opacity-50"
+    format="webp"
+    sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
+    :width="1920"
+    :height="1080"
+  />
 </template>
