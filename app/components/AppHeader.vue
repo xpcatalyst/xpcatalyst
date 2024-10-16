3ce065407f4798a42ae455bebbda0ee225131feb
Jérôme Abel <contact@jeromeabel.net>
2024-10-16 06:58:20 +0200

    ui: colors and hover underline on header links

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -4,24 +4,29 @@
       <nav class="flex gap-16 items-center">
         <NuxtLink
           to="/"
-          class="flex items-center gap-3 font-bold"
+          class="flex items-center gap-3 font-extrabold font-readex  hover:underline underline-offset-2"
         >
           <Icon
-            name="xp:logo2"
-            class="text-2xl font-readex"
-          /> XP Catalyst
+            name="xp:logo"
+            class="text-2xl"
+          /> XPCATALYST
         </NuxtLink>
-
         <NuxtLink
           to="/projects"
+          class="hover:underline underline-offset-2"
         >
           Projects
         </NuxtLink>
-        <NuxtLink to="/campus">
+        <NuxtLink
+          to="/campus"
+          class="hover:underline underline-offset-2"
+        >
           Campus
         </NuxtLink>
       </nav>
-      <AuthButtons />
+      <div class="flex items-center  rounded-full bg-neutral-100">
+        <AuthButtons />
+      </div>
     </div>
   </header>
 </template>
--- layers/auth/presentation/components/AuthButtons.vue
+++ layers/auth/presentation/components/AuthButtons.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="flex gap-8 items-center">
-    <NuxtLink to="/login">
-      Login
-    </NuxtLink>
-    <NuxtLink
-      to="/subscribe"
-      class="px-6 py-2 rounded-full bg-black text-white flex items-center gap-2"
-    >
-      Join us
-    </NuxtLink>
-  </div>
+  <NuxtLink
+    to="/login"
+    class="ps-6 py-2 pe-3 text-neutral-500 hover:text-black hover:underline underline-offset-2"
+  >
+    Login
+  </NuxtLink>
+  <NuxtLink
+    to="/subscribe"
+    class="px-6 py-2 rounded-full bg-black text-white hover:underline underline-offset-2"
+  >
+    Join us
+  </NuxtLink>
 </template>
