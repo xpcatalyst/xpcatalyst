c5e2887ce73364a4de93dffbfd092b9355b89f08
Jérôme Abel <contact@jeromeabel.net>
2024-10-20 08:55:52 +0200

    tests(newsletter): final tests for in memory repository

---
+++ layers/newsletter/repositories/in-memory-repository.ts
@@ -0,0 +1,49 @@
+// Repositories (Secondary Adapters)
+// They adapt external resources (databases, APIs) into your domain
+
+import type { INewsletterRepository } from '../domain/ports/newsletter-repository-interface'
+import type { Subscriber } from '../domain/entities/subscriber'
+import { failure, success } from '~/shared/result'
+
+export const ERRORS = {
+  SUBSCRIBER_NOT_FOUND: 'Subscriber not found in the database',
+  // DATABASE_ERROR: 'A database error occurred',
+  DUPLICATE_EMAIL: 'Email already exists in the database',
+  // CONNECTION_FAILED: 'Failed to connect to the database',
+  // QUERY_FAILED: 'Database query failed',
+  // TRANSACTION_FAILED: 'Database transaction failed',
+} as const
+
+export const createInMemoryRepository = (): INewsletterRepository => {
+  const subscribers: Subscriber[] = []
+  let currentId = 1
+
+  const addSubscriber = async (email: Email) => {
+    // No domain-specific validation like email verification => Use Case
+    const existingSubscriber = await getSubscriberByEmail(email)
+    if (existingSubscriber.success) {
+      return failure(ERRORS.DUPLICATE_EMAIL)
+    }
+
+    const newSubscriber: Subscriber = { id: currentId++, email }
+
+    subscribers.push(newSubscriber)
+    return success(newSubscriber)
+  }
+
+  const getSubscriberByEmail = async (email: Email): Promise<Result<Subscriber>> => {
+    const subscriber = subscribers.find(subscriber => subscriber.email === email)
+    return subscriber ? success(subscriber) : failure(ERRORS.SUBSCRIBER_NOT_FOUND)
+  }
+
+  const subscribe = async (_email: Email) => {
+    return failure('Not yet implemented')
+  }
+
+  return {
+    addSubscriber,
+    getSubscriberByEmail,
+    subscribe,
+
+  }
+}
