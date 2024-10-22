ce15800832828afbf99fdce520ce2dd3dee4ee4c
Jérôme Abel <contact@jeromeabel.net>
2024-10-22 08:16:37 +0200

    tests(NewsletterForm): add render form and disabled submit states

--- layers/newsletter/components/NewsletterForm.vue
+++ layers/newsletter/components/NewsletterForm.vue
@@ -1,38 +1,13 @@
 <script setup lang="ts">
-import { useForm, ErrorMessage, configure } from 'vee-validate'
-import { toTypedSchema } from '@vee-validate/zod'
-import * as z from 'zod'
-
+import { useNewsletter } from '../composables/useNewsletter'
 import { Button } from '@/components/ui/button'
-import {
-  FormControl,
-  FormField,
-  FormItem,
-} from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 
-configure({
-  validateOnBlur: true, // controls if `blur` events should trigger validation with `handleChange` handler
-  validateOnChange: false, // controls if `change` events should trigger validation with `handleChange` handler
-  validateOnInput: false, // controls if `input` events should trigger validation with `handleChange` handler
-  validateOnModelUpdate: false, // controls if `update:modelValue` events should trigger validation with `handleChange` handler
-})
+const { isButtonDisabled, success, email } = useNewsletter()
 
-// Define your schema
-const formSchema = toTypedSchema(z.object({
-  email: z.string().email().min(2).max(50),
-}))
-
-const { isFieldDirty, handleSubmit } = useForm({
-  validationSchema: formSchema,
-})
-
-// Form submission
-const onSubmit = handleSubmit((values, actions) => {
-  console.log(values)
-
-  actions.resetField('email')
-})
+const onSubmit = () => {
+  console.log('values')
+}
 </script>
 
 <template>
@@ -40,41 +15,32 @@ const onSubmit = handleSubmit((values, actions) => {
     Subscribe to the newsletter to follow the progress.
   </p>
   <form
-    class="flex items-center group focus-within:ring-2 rounded-3xl focus-within:ring-offset-2 focus-within:ring-black"
+    class="flex items-center focus-within:ring-2 rounded-3xl focus-within:ring-offset-2 focus-within:ring-black"
+    data-testid="newsletter-form"
     @submit="onSubmit"
   >
-    <FormField
-      v-slot="{ componentField, meta }"
-      name="email"
-      :validate-on-blur="!isFieldDirty"
-    >
-      <FormItem>
-        <FormControl>
-          <Input
-            type="text"
-            placeholder="Enter your email"
-            v-bind="componentField"
-            :class="{
-              'text-foreground border-green-500': meta.valid && meta.dirty,
-            }"
-            class="rounded-s-3xl rounded-e-none ps-6 w-80 focus-visible:ring-0 text-lg"
-          />
-        </FormControl>
-      </FormItem>
-    </FormField>
+    <Input
+      v-model="email"
+      placeholder="Enter your email"
+      data-testid="newsletter-email"
+      :class="{
+        'text-foreground border-green-500': success,
+      }"
+      class="rounded-s-3xl rounded-e-none ps-6 w-80 focus-visible:ring-0 text-lg"
+    />
     <Button
       type="submit"
+      data-testid="newsletter-submit"
       class="rounded-e-3xl rounded-s-none pe-6 text-lg"
+      :disabled="isButtonDisabled"
     >
       Stay Updated
     </Button>
   </form>
   <p
     class="py-2 h-6"
+    data-testid="newsletter-message"
   >
-    <ErrorMessage
-      name="email"
-      class="text-destructive"
-    />
+    message
   </p>
 </template>
---
+++ layers/newsletter/tests/component/NewsletterForm.spec.ts
@@ -0,0 +1,36 @@
+// @vitest-environment nuxt
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import NewsletterForm from '../../components/NewsletterForm.vue'
+
+describe('NewsletterForm', () => {
+  const wrapper = mount(NewsletterForm)
+
+  const findForm = () => wrapper.find('[data-testid="newsletter-form"]')
+  const findEmail = () => wrapper.find('[data-testid="newsletter-email"]')
+  const findSubmitButton = () => wrapper.find('[data-testid="newsletter-submit"]')
+  const findMessage = () => wrapper.find('[data-testid="newsletter-message"]')
+
+  it('should render the form correctly', () => {
+    expect(findForm().exists()).toBe(true)
+    expect(findEmail().exists()).toBe(true)
+    expect(findSubmitButton().exists()).toBe(true)
+    expect(findMessage().exists()).toBe(true)
+  })
+
+  it('should disable the button when email field is empty and enables when filled ', async () => {
+    await findEmail().setValue('')
+    expect(findSubmitButton().attributes('disabled')).toBe('')
+
+    await findEmail().setValue('not empty')
+    expect(findSubmitButton().attributes('disabled')).toBeUndefined()
+  })
+})
+
+/*
+4. `it('emits input event when email is typed')`
+5. `it('emits submit event on button click')`
+6. `it('disables submit button when "disabled" prop is true')`
+7. `it('shows error message when "error" prop is passed')`
+8. `it('shows loading spinner when "loading" prop is true')`
+*/
