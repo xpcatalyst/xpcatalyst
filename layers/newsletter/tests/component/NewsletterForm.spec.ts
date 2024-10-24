0572bf8856bc5e5ce1416d5a5c3e38574a7271b1
Jérôme Abel <contact@jeromeabel.net>
2024-10-24 06:02:08 +0200

    tests(newsletter): add required, messages rendering tests

--- layers/newsletter/tests/component/NewsletterForm.spec.ts
+++ layers/newsletter/tests/component/NewsletterForm.spec.ts
@@ -1,36 +1,61 @@
 // @vitest-environment nuxt
-import { describe, it, expect } from 'vitest'
-import { mount } from '@vue/test-utils'
+import { describe, it, expect, beforeEach } from 'vitest'
+import type { VueWrapper } from '@vue/test-utils'
+import { flushPromises, mount } from '@vue/test-utils'
 import NewsletterForm from '../../components/NewsletterForm.vue'
+import { SUCCESS } from '../../composables/useNewsletter'
+import { EMAIL_ERRORS } from '~/shared/email'
 
 describe('NewsletterForm', () => {
-  const wrapper = mount(NewsletterForm) // We need mount (or mountSuspended) instead of shallow mount to use Input, Button Shadcn elements
+  let wrapper: VueWrapper
 
+  beforeEach(() => {
+    // The component uses Input, Button From Shadcn/ui
+    // We need mount (or mountSuspended) instead of shallow mount
+    // Nuxt testing environment
+    wrapper = mount(NewsletterForm)
+  })
+
+  // Helpers
   const findForm = () => wrapper.find('[data-testid="newsletter-form"]')
-  const findEmail = () => wrapper.find('[data-testid="newsletter-email"]')
+  const findEmailInput = () => wrapper.find('[data-testid="newsletter-email"]')
   const findSubmitButton = () => wrapper.find('[data-testid="newsletter-submit"]')
   const findMessage = () => wrapper.find('[data-testid="newsletter-message"]')
 
   it('should render the form correctly', () => {
     expect(findForm().exists()).toBe(true)
-    expect(findEmail().exists()).toBe(true)
+    expect(findEmailInput().exists()).toBe(true)
     expect(findSubmitButton().exists()).toBe(true)
     expect(findMessage().exists()).toBe(true)
   })
 
   it('should disable the button when email field is empty and enables when filled', async () => {
-    await findEmail().setValue('')
-    expect(findSubmitButton().attributes('disabled')).toBe('') // disabled = true
+    await findEmailInput().setValue('')
+    expect(findSubmitButton().attributes('disabled')).toBeDefined() // disabled = true
 
-    await findEmail().setValue('not empty')
+    await findEmailInput().setValue('not empty')
     expect(findSubmitButton().attributes('disabled')).toBeUndefined() // disabled = false
   })
-})
 
-/*
-4. `it('emits input event when email is typed')`
-5. `it('emits submit event on button click')`
-6. `it('disables submit button when "disabled" prop is true')`
-7. `it('shows error message when "error" prop is passed')`
-8. `it('shows loading spinner when "loading" prop is true')`
-*/
+  it('should have required attribute on email input', () => {
+    expect(findEmailInput().attributes('required')).toBeDefined()
+  })
+
+  describe('on form submit, when username and password are filled', () => {
+    it('should render an error message on invalid email', async () => {
+      await findEmailInput().setValue('invalid-email')
+      expect(findMessage().text()).toBe('')
+      await findForm().trigger('submit')
+      expect(findEmailInput().classes()).toContain('border-error')
+      expect(findMessage().text()).toBe(EMAIL_ERRORS.INVALID)
+    })
+
+    it('should render a success message on valid email', async () => {
+      await findEmailInput().setValue('valid@example.com')
+      await findForm().trigger('submit')
+      await flushPromises()
+      expect(findEmailInput().classes()).toContain('border-success')
+      expect(findMessage().text()).toBe(SUCCESS)
+    })
+  })
+})
