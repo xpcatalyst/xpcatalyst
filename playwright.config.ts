91e64d9961bb27a4ed49985427f1fa018d0e48e2
Jérôme Abel <contact@jeromeabel.net>
2024-10-27 18:16:09 +0100

    test(ci): e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,4 +3,7 @@ import type { ConfigOptions } from '@nuxt/test-utils/playwright'
 
 export default defineConfig<ConfigOptions>({
   testMatch: '**/__tests__/e2e/*.spec.ts',
+  use: {
+    baseURL: process.env.BASE_URL,
+  },
 })
