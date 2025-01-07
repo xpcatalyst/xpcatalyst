05f499f8e3e25ecaf5dead18030beed92bb21957
Jérôme Abel <contact@jeromeabel.net>
2025-01-07 13:15:38 +0100

    feat: add CtaButton to home page

---
+++ app/components/ui/button/CtaButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { ButtonVariants } from '.';
+
+const props = defineProps<{
+    to: string,
+    icon?: string,
+    variant?: ButtonVariants['variant']
+    size?: ButtonVariants['size']
+}>()
+
+
+const classes = computed(() => `text-${props.size}`)
+</script>
+
+<template>
+    <NuxtLink :to="to" class="inline-block ">
+        <Button class="rounded-full flex items-center gap-2" :class="classes" :variant="variant" :size="size">
+            <slot />
+            <Icon v-if="icon" :name="icon" />
+        </Button>
+
+    </NuxtLink>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import CtaButton from '~/components/ui/button/CtaButton.vue';
+</script>
+
 <template>
   <section class="flex items-center flex-1">
     <div class="container flex flex-col gap-16">
@@ -5,9 +9,13 @@
         Secure your career in web development & make a positive impact by working on curated
         <span class="text-purple-300 font-semibold">real-world projects</span> for non-profit initiatives.
       </p>
-      <div class="flex gap-16">
-        <nuxt-link to="/projects" class="btn btn-primary">Explore Projects</nuxt-link>
-        <nuxt-link to="/guide/getting-started" class="btn btn-secondary">Get Started</nuxt-link>
+      <div class="flex gap-8">
+        <CtaButton to="/" variant="default" size="lg" icon="ph:arrow-right-bold">
+          Get started
+        </CtaButton>
+        <CtaButton to="/" variant="secondary" size="lg">
+          Explore Projects
+        </CtaButton>
       </div>
     </div>
   </section>
