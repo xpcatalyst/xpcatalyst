2b03f444a436ee2cc9e50fe013cfb9d2296ca23b
Jérôme Abel <contact@jeromeabel.net>
2025-01-09 07:02:19 +0100

    style(CtaButton): add translate animation to icon

--- app/components/ui/button/CtaButton.vue
+++ app/components/ui/button/CtaButton.vue
@@ -8,16 +8,14 @@ const props = defineProps<{
     size?: ButtonVariants['size']
 }>()
 
-
 const classes = computed(() => `text-${props.size}`)
 </script>
 
 <template>
-    <NuxtLink :to="to" class="inline-block ">
+    <NuxtLink :to="to" class="inline-block group">
         <Button class="rounded-full" :class="classes" :variant="variant" :size="size">
             <slot />
-            <Icon v-if="icon" :name="icon" class="ms-2" />
+            <Icon v-if="icon" :name="icon" class="ms-2 group-hover:translate-x-2 transition-transform" />
         </Button>
-
     </NuxtLink>
 </template>
