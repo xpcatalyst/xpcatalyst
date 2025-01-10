a754187fe9f47ba2efb7bffa6352e2830acf145a
Jérôme Abel <contact@jeromeabel.net>
2025-01-10 13:03:48 +0100

    style: x icon hover

--- app/components/base/AppVersion.vue
+++ app/components/base/AppVersion.vue
@@ -1,13 +1,18 @@
 <script lang="ts" setup>
-const config = useRuntimeConfig()
-const open = ref(true)
+const config = useRuntimeConfig();
+const open = ref(true);
 </script>
 
 <template>
   <div v-if="open" class="w-screen bg-purple-100 flex py-4 px-8 text-black">
     <div class="container flex justify-center">
-      <p>Prototype v.<span class="font-semibold">{{ config.public.version }}</span></p>
+      <p>
+        Prototype v.<span class="font-semibold">{{ config.public.version }}</span>
+      </p>
     </div>
-    <Icon name="ph:x" size="20" class="hover:cursor-pointer" @click="() => open = false" />
+
+    <div class="h-8 w-8 group hover:bg-white/50 hover:cursor-pointer flex justify-center items-center rounded-full">
+      <Icon name="ph:x" size="20" class="" @click="() => (open = false)" />
+    </div>
   </div>
 </template>
