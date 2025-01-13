797622cbd16b823084f3d2cf3d5e462b6f068f84
Jérôme Abel <contact@jeromeabel.net>
2025-01-13 06:35:34 +0100

    style: add heart icon to footer

--- app/components/base/AppFooter.vue
+++ app/components/base/AppFooter.vue
@@ -3,7 +3,8 @@
     <div class="container flex flex-col gap-16">
       <div class="text-center text-sm">
         <p>
-          Made with ❤️ by
+          Made with
+          <Icon name="ph:heart" class="bg-purple-500" /> by
           <NuxtLink to="https://dev.jeromeabel.net">J&eacute;r&ocirc;me Abel</NuxtLink> -
           <NuxtLink to="https://github.com/xpcatalyst/xpcatalys">Source Code</NuxtLink>
         </p>
