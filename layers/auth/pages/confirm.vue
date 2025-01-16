169fda64256e8e3d3b62311b0d7353c38102ec93
Jérôme Abel <contact@jeromeabel.net>
2025-01-16 06:39:39 +0100

    style: center waiting for login in confirm page

--- layers/auth/pages/confirm.vue
+++ layers/auth/pages/confirm.vue
@@ -3,12 +3,13 @@ const user = useSupabaseUser()
 
 watch(user, () => {
   if (user.value) {
-    // Redirect to protected page
     return navigateTo('/')
   }
 }, { immediate: true })
 </script>
 
 <template>
-  <div>Waiting for login...</div>
+  <div class="grid items-center justify-center h-full text-xl animate-pulse">
+    Waiting for login...
+  </div>
 </template>
