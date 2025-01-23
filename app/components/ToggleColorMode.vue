bf0dcef1005dd7dd860645b54c341e7eca59ba5e
Jérôme Abel <contact@jeromeabel.net>
2025-01-23 11:46:07 +0100

    ui: toggle color mode aria and styles

--- app/components/ToggleColorMode.vue
+++ app/components/ToggleColorMode.vue
@@ -1,21 +1,24 @@
 <script lang="ts" setup>
 const colorMode = useColorMode()
-const toggle = () => colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark'
+const toggleTheme = () => {
+  colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark'
+}
 </script>
 
 <template>
-  <Button
-    :variant="null"
-    @click="toggle"
+  <button
+    type="button"
+    aria-label="Toggle theme"
+    class="relative flex items-center justify-center"
+    @click="toggleTheme"
   >
     <Icon
       name="ph:moon-fill"
-      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+      class="h-5 w-5 opacity-100 dark:opacity-0 transition-all"
     />
     <Icon
       name="ph:sun-fill"
-      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+      class="absolute h-5 w-5 opacity-0 dark:opacity-100 transition-all"
     />
-    <span class="sr-only">Toggle theme</span>
-  </Button>
+  </button>
 </template>
