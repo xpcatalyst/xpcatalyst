e4e4c2b5f3a20cf8869a2868336ebd6d8802d46f
Jérôme Abel <contact@jeromeabel.net>
2025-01-23 16:11:32 +0100

    chore: add workspace

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,27 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    include: ['layers/*/__tests__/unit/*.spec.ts'],
-    environment: 'node',
+    // include: ['layers/*/__tests__/unit/*.spec.ts'],
+    // environment: 'node',
+    workspace: [
+      'layers/*',
+
+      {
+        extends: true,
+        test: {
+          include: ['__tests__/components/*.spec.ts'],
+          // it is recommended to define a name when using inline configs
+          name: 'happy-dom',
+          environment: 'happy-dom',
+        },
+      },
+      {
+        test: {
+          include: ['__tests__/unit/*.spec.ts'],
+          name: 'node',
+          environment: 'node',
+        },
+      },
+    ],
   },
 })
