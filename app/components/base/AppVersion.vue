30cf9d3985aabb134fc804f94c1d80810853796c
Jérôme Abel <contact@jeromeabel.net>
2025-01-24 11:39:51 +0100

    feat(banner): add local storage to handle visibility between refreshed pages

--- app/components/base/AppVersion.vue
+++ app/components/base/AppVersion.vue
@@ -1,11 +1,24 @@
-<script lang="ts" setup>
+<script setup lang="ts">
 const config = useRuntimeConfig()
-const open = ref(true)
+const showBanner = ref(false)
+
+const closeBanner = () => {
+  showBanner.value = false
+  localStorage.setItem('banner-version', config.public.version)
+}
+
+onMounted(() => {
+  const lastDismissedVersion = localStorage.getItem('banner-version')
+
+  if (lastDismissedVersion !== config.public.version) {
+    showBanner.value = true
+  }
+})
 </script>
 
 <template>
   <div
-    v-if="open"
+    v-if="showBanner"
     class="w-screen bg-purple-100 dark:bg-purple-700 flex py-4 px-8 text-black dark:text-gray-200 items-center"
   >
     <div class="container flex justify-center">
@@ -14,12 +27,13 @@ const open = ref(true)
       </p>
     </div>
 
-    <div class="h-8 w-8 group hover:bg-white/20 hover:cursor-pointer flex justify-center items-center rounded-full">
+    <div
+      class="h-8 w-8 group hover:bg-white/20 hover:cursor-pointer flex justify-center items-center rounded-full"
+      @click="closeBanner"
+    >
       <Icon
         name="ph:x"
         size="20"
-        class=""
-        @click="() => (open = false)"
       />
     </div>
   </div>
