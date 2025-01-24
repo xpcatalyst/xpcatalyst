39159678b754837d32e18a11d38321a1d595c3c5
Jérôme Abel <contact@jeromeabel.net>
2025-01-24 16:25:50 +0100

    chore: remove lint rules with pages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,11 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
+  {
+    files: ['**/pages/**/*.vue'],
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/no-multiple-template-root': 'off',
+    },
+  },
 )
