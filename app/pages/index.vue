0c77ce605a57309ac7f2d81302166f2165e9edf7
Jérôme Abel <contact@jeromeabel.net>
2025-01-24 13:07:26 +0100

    style: home page title

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,8 +5,8 @@ import CtaButton from '~/components/ui/button/CtaButton.vue'
 <template>
   <section class="flex items-center flex-1">
     <div class="container flex flex-col gap-12">
-      <p class="max-w-4xl text-pretty text-5xl leading-tight">
-        <span class="font-semibold">Let's catalyze experience together!</span> <br> Build real-world projects & <br>help non-profit initiatives.
+      <p class="max-w-4xl text-pretty text-6xl leading-tight">
+        <span class="font-bold uppercase text-purple-500 dark:text-purple-400 text-4xl tracking-wider">Let's catalyze experience together!</span> <br>Build real-world projects &<br> help non-profit initiatives.
       </p>
       <div class="flex gap-8">
         <CtaButton
