344f52f984097ff66d27d6020b9ec4b813fe980c
Jérôme Abel <contact@jeromeabel.net>
2025-01-26 08:18:47 +0100

    style: home page new title and colors to cta buttons

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,30 +4,37 @@ import CtaButton from '~/components/ui/button/CtaButton.vue'
 
 <template>
   <section class="flex items-center flex-1">
-    <div class="container flex flex-col gap-12">
-      <p class="max-w-4xl text-pretty text-6xl leading-tight">
-        <span class="font-bold uppercase tracking-wide">Let's catalyze experience together!</span> <br>Build real-world projects &<br> help non-profit initiatives.
-      </p>
-      <div class="flex gap-8">
-        <CtaButton
-          to="/"
-          variant="secondary"
-          size="lg"
-        >
-          Explore Projects
-        </CtaButton>
-        <CtaButton
-          to="/"
-          size="lg"
-          class="group flex items-center"
-        >
-          Get started
-          <Icon
-            name="ph:arrow-right"
-            class="group-hover:translate-x-2 transition-transform"
-            size="24"
-          />
-        </CtaButton>
+    <div class="container">
+      <div class="max-w-2xl flex flex-col gap-12">
+        <div class="text-pretty space-y-4">
+          <h2 class="text-2xl font-semibold">
+            Build real-world projects & help non-profit initiatives
+          </h2>
+          <h1 class="font-bold text-7xl leading-tight">
+            Gain Experience in Web Development
+          </h1>
+        </div>
+        <div class="flex gap-8">
+          <CtaButton
+            to="/"
+            size="lg"
+            class="bg-green-300"
+          >
+            Explore Projects
+          </CtaButton>
+          <CtaButton
+            to="/"
+            size="lg"
+            class="group flex items-center bg-purple-400"
+          >
+            Get started
+            <Icon
+              name="ph:arrow-right"
+              class="group-hover:translate-x-2 transition-transform"
+              size="24"
+            />
+          </CtaButton>
+        </div>
       </div>
     </div>
   </section>
