cd34ddd2b879228495d9bf2deffd514e16678320
Jérôme Abel <contact@jeromeabel.net>
2025-01-26 09:10:43 +0100

    style(login): add quote

--- layers/auth/pages/login.vue
+++ layers/auth/pages/login.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { useAuth } from '../composables/useAuth'
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
 
 definePageMeta({
   layout: false,
@@ -13,32 +14,65 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="relative h-screen w-screen flex items-center justify-center bg-gradient-to-r from-black to-purple-950">
-    <div class="px-24 pt-16 pb-16 rounded-2xl bg-purple-800 text-white shadow-lg max-w-2xl text-center z-10">
-      <h1 class="flex items-center justify-center text-3xl font-semibold mb-2">
-        <Icon
-          name="xpc:logo"
-          class="w-8 h-8 mr-2"
-        />
-        XP.CATALYST
-      </h1>
-      <p class="text-lg mb-8">
-        Welcome back! <br> Please sign in using your GitHub account.
-      </p>
-      <div class="flex flex-col items-center justify-center gap-8">
-        <LoginButton
-          :loading="loading"
-          :label="loginGithubText"
-          size="lg"
-          class="gap-4"
-          @login="signIn"
-        />
+  <div class="h-screen w-screen flex">
+    <div class="dark:bg-neutral-800 bg-neutral-500 p-16 w-1/2 lg:full">
+      <header>
         <NuxtLink
           to="/"
-          class="hover:underline"
+          class="font-bold tracking-wide flex items-center gap-4 text-2xl uppercase"
         >
-          Back to Home
+          <Icon name="xpc:logo" /> Xp.Catalyst
         </NuxtLink>
+      </header>
+      <section class="flex items-center justify-center h-full">
+        <div class="max-w-lg">
+          <div class="flex flex-col gap-6 border rounded-xl p-8 border-muted-foreground">
+            <div>
+              <h1 class="text-2xl font-semibold tracking-tight">
+                Welcome back!
+              </h1>
+              <p class="text-sm text-muted-foreground">
+                Sign in to your account
+              </p>
+            </div>
+
+            <LoginButton
+              :loading="loading"
+              :label="loginGithubText"
+              size="lg"
+              @login="signIn"
+            />
+          </div>
+          <p class="text-sm text-muted-foreground mt-4">
+            Don't have an account? <NuxtLink
+              to="/register"
+              class="underline underline-offset-4 hover:text-primary"
+            >
+              Register here
+            </NuxtLink>
+          </p>
+        </div>
+      </section>
+    </div>
+
+    <div class="flex justify-center items-center flex-1 p-24">
+      <div class="flex flex-col gap-6">
+        <blockquote class="italic text-3xl font-serif max-w-2xl relative before:content-['\201C'] before:text-8xl before:text-neutral-600 before:absolute before:-top-10 before:-left-12">
+          Many job postings require 2+ years of experience. XP Catalyst is designed to bridge the experience gap by providing a community platform where you can build real-world projects in collaborative teams."
+        </blockquote>
+        <footer class="text-sm text-muted-foreground flex items-center gap-4 ms-16">
+          <Avatar>
+            <AvatarImage
+              src="/jerome.png"
+              alt="User Avatar"
+            />
+            <AvatarFallback>JA</AvatarFallback>
+          </Avatar>
+          <div>
+            <p>Jérôme Abel,</p>
+            <p>Founder of xpatalyst.dev</p>
+          </div>
+        </footer>
       </div>
     </div>
   </div>
